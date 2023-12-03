6c74f63fc39b312b862e8cf630e8ff77591037e8
studyhallnext <studyhallnext@gmail.com>
2023-12-02 22:24:23 -0500

    web

--- utility/serverFunctions/handleAll.ts
+++ utility/serverFunctions/handleAll.ts
@@ -3,11 +3,15 @@
 import { usableDb } from "@/db/index";
 import * as schema from "@/db/schema"
 
+
 export default async function deleteAll() {
-    // return
+    return
     console.log(`$deleted all`);
     await usableDb.delete(schema.posts);
     await usableDb.delete(schema.comments);
     await usableDb.delete(schema.communities);
     await usableDb.delete(schema.replies);
+    await usableDb.delete(schema.usersToLikedPosts);
+    await usableDb.delete(schema.usersToLikedComments);
+    await usableDb.delete(schema.usersToLikedReplies);
 }
